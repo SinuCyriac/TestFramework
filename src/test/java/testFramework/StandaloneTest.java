e1ef8ff51d69d73b983c481d3e1c9e104be4ba61
SinuCyriac <122955028+SinuCyriac@users.noreply.github.com>
2023-06-12 01:43:07 -0700

    create framework

--- src/test/java/testFramework/StandaloneTest.java
+++ src/test/java/testFramework/StandaloneTest.java
@@ -1,140 +1,62 @@
 package testFramework;
 
-import java.time.Duration;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementClickInterceptedException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import testFramework.PageObjects.Cart;
+import testFramework.PageObjects.MyOrders;
+import testFramework.PageObjects.OrderConfirm;
+import testFramework.PageObjects.PlaceOrder;
+import testFramework.PageObjects.Products;
+import testFramework.TestComponents.BaseTest;
 
-public class StandaloneTest {
+public class StandaloneTest extends BaseTest {
 
-	public static void main(String[] args) throws InterruptedException {
+	@Test
+	public void submitOrder() throws IOException, InterruptedException {
 		// Driver initialization
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		WebDriver driver = new ChromeDriver(options);
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		String baseUrl = "https://rahulshettyacademy.com/client";
 
-		driver.get(baseUrl);
-
+		
 		// login page
 		String username = "sinucyriac@test.com";
 		String password = "Test@1234";
-		driver.findElement(By.id("userEmail")).sendKeys(username);
-		driver.findElement(By.id("userPassword")).sendKeys(password);
-		driver.findElement(By.className("login-btn")).click();
 
-		// product catalogue page
+		Products prod = lan.userLogin(username, password);
 
-		List<WebElement> products = driver.findElements(By.className("mb-3"));
-
-		int itemsCount = products.size();
-
-		// add item to cart
+		// product catalog page
 		String itemsToBuy = "adidas original";
-		for (int i = 0; i < itemsCount; i++) {
 
-			if (products.get(i).findElement(By.cssSelector(".card-body h5")).getText().equalsIgnoreCase(itemsToBuy)) {
+		prod.addProdsToCart(itemsToBuy);
 
-				products.get(i).findElement(By.cssSelector(".card-body button:last-of-type")).click();
-				break;
-
-			}
-
-		}
 		// Click on cart
-//		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(30));
-//		wait.until(ExpectedConditions.visibilityOf(cartBtn));
-		WebElement cartBtn = driver.findElement(By.className("fa-shopping-cart"));
-		Thread.sleep(3000L);
-
-		try {
-			cartBtn.click();
-		} catch (ElementClickInterceptedException e) {
-			e.printStackTrace();
-
-			System.out.println("inside catch");
-
-		}
+		Cart cart = prod.clickOnCart();
 
 		// verify cart
-		List<WebElement> cartItems = driver.findElements(By.cssSelector(".cartSection h3"));
-		for (WebElement item : cartItems) {
-			String cartItem = item.getText();
-//			System.out.println(cartItem);
-			if (item.getText().equalsIgnoreCase(itemsToBuy)) {
 
-				Assert.assertTrue(true, "item found");
-				break;
-			}
-		}
-
+		Boolean match = cart.verifyCart(itemsToBuy);
+		Assert.assertTrue(match);
 		// checkout button
-		driver.findElement(By.cssSelector(".totalRow button")).click();// '//*[contains(text(), "Tired")]'
+		PlaceOrder place = cart.checkOutOrder();
 
 		// checkout page
-
-		driver.findElement(By.xpath("//input[@placeholder='Select Country']")).sendKeys("Ind");
-		List<WebElement> listedCountries = driver.findElements(By.cssSelector(".ta-item span"));
-
-		for (WebElement country : listedCountries) {
-
-			if (country.getText().equalsIgnoreCase("India")) {
-				country.click();
-				break;
-			}
-		}
-
+		place.addDetails();
 		// place order button
-		driver.findElement(By.className("action__submit")).click();
+		OrderConfirm confirm = place.placeOrder();
 
 		// order detail page
-		String ordMsg = driver.findElement(By.tagName("h1")).getText();
-
+		String ordMsg = confirm.confirmOrder();
 		Assert.assertEquals(ordMsg, "THANKYOU FOR THE ORDER.");
-		List<WebElement> orderNos = driver
-				.findElements(By.cssSelector("td[class='em-spacer-1'] label[class='ng-star-inserted']"));
+		ArrayList<String> orderIds = confirm.storeOrderId();
+		MyOrders orderHis = confirm.clickOnOrders();
 
-		ArrayList<String> orderIds = new ArrayList<String>();
-		for (WebElement orderNo : orderNos) {
-			String textId = orderNo.getText();
-			int firstInd = textId.indexOf("|") + 2;
-			int lastInd = textId.indexOf(" |");
-			String id = textId.substring(firstInd, lastInd);
-			orderIds.add(id);
-
-		}
-
-		// order history
-		driver.findElement(By.className("fa-handshake-o")).click();
-		List<WebElement> orderHis = driver.findElements(By.cssSelector(".table-bordered .ng-star-inserted th"));
-		for (WebElement id : orderHis) {
-			String oId = id.getText();
-
-			if (orderIds.contains(oId)) {
-//				System.out.println("order found");
-				Assert.assertTrue(true);
-			}
-
-		}
+		// order history page
+		Boolean check = orderHis.verifyOrder(orderIds);
+		Assert.assertTrue(check);
 
 		// tearDown
-		driver.close();
 	}
 
 }
